1bd7a51e369a19e327f6ad3170054ea5011ec954
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-03-29 00:44:24 -0300

    Teste flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-export default {
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
     // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
     // pages: './src/pages', // Path to Astro components, pages, and data
     // dist: './dist',       // When running `astro build`, path to final static output
     // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
     buildOptions: {
-        // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+        site: 'https://site-vallorx-astro.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
         sitemap: true,         // Generate sitemap (set to "false" to disable)
     },
     devOptions: {
@@ -14,4 +23,7 @@ export default {
     renderers: [
         "@astrojs/renderer-react",
     ],
-};
+    vite: {
+        plugins: ['flag-icons/css/flag-icons.min.css']
+    }
+});
