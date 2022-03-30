00474dff528cbe97ca4b001e67c157e5415f41ad
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-03-30 01:05:43 -0300

    refatorando video

--- public/js/videos.php
+++ public/js/videos.php
@@ -1,21 +1,24 @@
 <?php
-    $dir = '../assets/videos-news';
-    if (is_dir($dir)) {
-        $files = array_diff(scandir($dir), array('.', '..'));
-        // do something
-    } else {
-        echo "No such directory";
-    }
-    // $files2 = scandir($dir, 1);
+    if (isset($_GET)):
+        $dir = '../assets/videos-news';
+        if (is_dir($dir)):
+            $files = array_diff(scandir($dir), array('.', '..'));
+            // do something
+        else:
+            echo "No such directory";
+        endif;
+        // $files2 = scandir($dir, 1);
 
-    // print_r($files);
-    // print_r($files2);
-    // echo $files
-    if ($files):
-        $array  = array('status' => '200', 'mensagem' => 'Sua mensagem foi enviada com sucesso!', 'data' => $files);
-        echo json_encode($array);
-    else:
-        $array  = array('status' => '500', 'mensagem' => 'Infelizmente houve um erro ao enviar sua mensagem!');
-        echo json_encode($array);
+        // print_r($files);
+        // print_r($files2);
+        // echo $files
+        if ($files):
+            $array  = array('status' => '200', 'mensagem' => 'Sua mensagem foi enviada com sucesso!', 'data' => $files);
+            echo json_encode($array);
+        else:
+            $array  = array('status' => '500', 'mensagem' => 'Infelizmente houve um erro ao enviar sua mensagem!');
+            echo json_encode($array);
+        endif;
+
     endif;
 ?>
