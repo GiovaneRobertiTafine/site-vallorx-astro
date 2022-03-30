88b0b6f66a6be6bfb60e23c8290a11aa3d9cd8d4
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-03-30 00:48:14 -0300

    Refatorando videos

--- public/js/videos.php
+++ public/js/videos.php
@@ -1,15 +1,16 @@
 <?php
-    $dir    = 'https://vallorx.com.br/videos/';
-    $files1 = scandir($dir);
+    $dir = 'https://vallorx.com.br/videos';
+    $files = array_diff(scandir($dir), array('.', '..'));
     // $files2 = scandir($dir, 1);
 
-    print_r($files1);
+    print_r($files);
     // print_r($files2);
-    if ($files1):
-        $array  = array('status' => '200', 'mensagem' => 'Sua mensagem foi enviada com sucesso!');
-        echo json_encode($array);
-    else:
-        $array  = array('status' => '500', 'mensagem' => 'Infelizmente houve um erro ao enviar sua mensagem!');
-        echo json_encode($array);
-    endif;
+    echo $files
+    // if ($files):
+    //     $array  = array('status' => '200', 'mensagem' => 'Sua mensagem foi enviada com sucesso!', 'data' => $files);
+    //     echo json_encode($array);
+    // else:
+    //     $array  = array('status' => '500', 'mensagem' => 'Infelizmente houve um erro ao enviar sua mensagem!');
+    //     echo json_encode($array);
+    // endif;
 ?>
--- src/js/obter-videos.js
+++ src/js/obter-videos.js
@@ -2,19 +2,20 @@ import $ from 'jquery';
 
 export async function obterVideos() {
     $.ajax({
+        headers: 'Cache-Control: no-cache',
         type: 'GET',
         url: '/js/videos.php',
         success: (result) => {
             console.log(result);
             if (result.status === '200') {
-                console.log(result.mensagem);
+                console.log(result);
             } else if (result.status === '400') {
                 $('#resposta-email').html(result.mensagem).css('color', 'red').show();
                 setTimeout(() => {
                     $('#resposta-email').hide();
                 }, 10000);
             } else if (result.status === '304') {
-                console.log(result.mensagem);
+                console.log(result);
             }
         },
         error: (result) => {
