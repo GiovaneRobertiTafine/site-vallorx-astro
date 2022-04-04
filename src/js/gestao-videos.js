e7d94727930e748a1bb4c75969907811b269684d
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-04-04 16:45:02 -0300

    reformulando requisicao

--- src/js/gestao-videos.js
+++ src/js/gestao-videos.js
@@ -10,42 +10,38 @@ export function addNews() {
     $('#btn-save').prop('disabled', true);
     $('.progress').css('display', 'block');
     const urlData = $('#form-add').serialize();
-    var i = setInterval(function () {
-        $.ajax({
-            type: 'POST',
-            url: 'https://vallorx.com.br/php/gestao-videos.php',
-            data: urlData,
-            success: (result) => {
-                console.log(result);
-                console.log(result.progress);
-                if (result.status === '200') {
-                    $('#resposta-add-news').html(result.mensagem).show();
-                    clearInterval(i);
-                    setTimeout(() => {
-                        $('#resposta-add-news').hide();
-                    }, 10000);
-                } else if (result.status === '400') {
-                    clearInterval(i);
-                    $('#resposta-add-news').html(result.mensagem).show();
-                    setTimeout(() => {
-                        $('#resposta-add-news').hide();
-                    }, 10000);
-                }
-            },
-            error: (result) => {
-                $('#resposta-add-news').html('Infelizmente houve um erro ao enviar sua mensagem!').show();
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://vallorx.com.br/php/gestao-videos.php',
+        data: urlData,
+        success: (result) => {
+            console.log(result.progress);
+            if (result.status === '200') {
+                $('#resposta-add-news').html(result.mensagem).show();
                 setTimeout(() => {
                     $('#resposta-add-news').hide();
                 }, 10000);
-
-            },
-            complete: () => { /* completo */
-                $('#form-contato').trigger('reset');
-                $('.progress').css('display', 'none');
+            } else if (result.status === '400') {
+                $('#resposta-add-news').html(result.mensagem).show();
+                setTimeout(() => {
+                    $('#resposta-add-news').hide();
+                }, 10000);
             }
+        },
+        error: (result) => {
+            $('#resposta-add-news').html('Infelizmente houve um erro ao enviar sua mensagem!').show();
+            setTimeout(() => {
+                $('#resposta-add-news').hide();
+            }, 10000);
 
-        });
-    }, 1000);
+        },
+        complete: () => { /* completo */
+            $('#form-contato').trigger('reset');
+            $('.progress').css('display', 'none');
+        }
+
+    });
 }
 
 const modal = document.getElementById('addModal');
