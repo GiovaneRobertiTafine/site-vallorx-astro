541d99f138eb20e3e75b7699674fc1801a7b9c6e
GiovaneRobertiTafine <kstv23@hotmail.com>
2026-07-21 15:07:08 -0300

    feat: retira serviço export import do nav menu.

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -74,7 +74,6 @@ const NavBar: React.FC<{ pageActive: number; }> = ({ pageActive }) => {
                                     <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                         <li><a className="dropdown-item" href={pageLinkServicos('#empresarial')}>Empresarial</a></li>
                                         <li><a className="dropdown-item" href={pageLinkServicos('#tributaria')}>Tributaria</a></li>
-                                        <li><a className="dropdown-item" href={pageLinkServicos('#export-import')}>Export | Import</a></li>
                                         <li><a className="dropdown-item" href={pageLinkServicos('#outsourcing')}>Outsourcing</a></li>
                                         <li><a className="dropdown-item" href={pageLinkServicos('#patrimonial')}>Patrimonial</a></li>
                                         <li><a className="dropdown-item" href={pageLinkServicos('#expatriados')}>Expatriados</a></li>
